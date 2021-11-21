98add93898e75af478f007849f8d0f36b0ae6d0b
Ninad Joshi <ninadgjoshi@gmail.com>
2021-11-21 17:42:10 +0530

    Version 1.2
    Changes:
    - Added grid search for DUC component
    - Styling changes for loader on takeUntil component

--- src/app/components/duc-component/duc-component.component.ts
+++ src/app/components/duc-component/duc-component.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable, of, Subscription } from 'rxjs';
-import { switchMap, distinctUntilChanged} from 'rxjs/operators';
+import { switchMap, distinctUntilChanged, filter, mergeMap, toArray, tap, startWith, map} from 'rxjs/operators';
+import { DataProviderService } from 'src/app/services/data-provider.service';
 
 @Component({
   selector: 'app-duc-component',
@@ -11,21 +12,43 @@ import { switchMap, distinctUntilChanged} from 'rxjs/operators';
 export class DucComponentComponent implements OnInit {
   public form$?: Observable<any>;
   public control$?: Observable<string>;
+  public person$? : Observable<any>;
   public sub: Subscription;
 
-  constructor() {
+  constructor(private dps: DataProviderService) {
     this.form$ = of(new FormGroup({
       'ip-control': new FormControl()
     }))
     this.form$ = this.form$.pipe(
       switchMap((form)=> {
-        this.control$ = form.get('ip-control').valueChanges.pipe(distinctUntilChanged((prev, curr)=>{
-          return (prev === curr) // if previous and current values are different then value passed to next observer
+        this.control$ = form.get('ip-control').valueChanges.pipe(
+          distinctUntilChanged((prev: string, curr:string)=>{
+            return (prev === curr) // if previous and current values are different then value passed to next observer
+        }),
+        tap((text: string)=> {
+          this.getPersonInfo(text);
         }))
         return of(form);
       }),
     )
     this.sub = new Subscription();
+  }
+
+  private getPersonInfo(text: string = ''): void {
+    this.person$ = this.dps.getPersonData().pipe(
+      mergeMap(data=> data['person']),
+      filter((person:any) => ((person.firstName.toUpperCase().startsWith(text.toUpperCase())))),
+      map((person: any)=>{
+        return person = {
+          ...person,
+          fullName: `${person.firstName} ${person.lastName}`
+        }
+      }),
+      toArray()
+    );
+  }
+
+  public restoreGrid(): void {
   }
 
   private ducExample() : void{
@@ -38,8 +61,9 @@ export class DucComponentComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getPersonInfo();
     // check console to see results
-    this.ducExample();
+    // this.ducExample();
   }
 
   ngAfterViewInit() {
